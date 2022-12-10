2592d30b93e5d3f99d092df197335f9672d59d0a
Lucas Músico <lucasmusico23@hotmail.com>
2022-12-10 11:12:53 -0300

    Adicionado exceção ao apertar cancelar

--- Módulo 1/Semana 02/Resolução Lista/M1S02_EX4.js
+++ Módulo 1/Semana 02/Resolução Lista/M1S02_EX4.js
@@ -2,8 +2,14 @@ var num = 0;
 var soma = 0;
 
 while(num != -1){
-    soma += parseInt(num);
-    num = window.prompt("Entre com um número para a soma. Digite -1 para finalizar.")
+    if (num !== null && !isNaN(num)){
+        console.log(num);
+        soma += parseInt(num);
+        num = window.prompt("Entre com um número para a soma. Digite -1 para finalizar.");
+    }
+    else {
+        num = 0;
+    }
 }
 
 window.alert(`A soma dos números é: ${soma}`);
