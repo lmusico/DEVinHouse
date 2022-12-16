1a822bfb7f975088b3ec093341e006b5f8057447
Lucas Músico <lucasmusico23@hotmail.com>
2022-12-16 20:02:30 -0300

    Atualização ex2

--- Módulo 1/Semana 03/Resolução Lista/EX2/app.js
+++ Módulo 1/Semana 03/Resolução Lista/EX2/app.js
@@ -1,4 +1,16 @@
-const mensagemOla = (nome) => console.log("Olá "+ nome);
+const mensagemOla = (nome) => `Olá ${nome}`;
 
-let ola = window.prompt("Qual o seu nome?");
-mensagemOla(ola);
+const entradaNome = document.getElementById('nome');
+const botaoAcao = document.getElementById('acao');
+const pMensagem = document.getElementById('mensagem');
+
+// let ola = window.prompt("Qual o seu nome?");
+// mensagemOla(ola);
+
+
+botaoAcao.addEventListener("click", () => {
+    const nome = entradaNome.value;
+    const msg = mensagemOla(nome);
+    pMensagem.innerHTML = msg;
+    
+})
