a01564d8fd89eaa1f045d88b082ab9949f5f12fc
Lucas Músico <lucasmusico23@hotmail.com>
2023-02-09 18:22:31 -0300

    EX5

---
+++ Módulo 1/Semana 06/EX5/EX5.js
@@ -0,0 +1,14 @@
+function obterClienteNoBancoDeDados(idCliente) {
+    return new Promise( (resolve, reject) => {
+            const cliente = { nome: 'bruce wayne', id: idCliente };
+            resolve(cliente);
+    });
+}
+
+async function processar() {
+    const cliente = await obterClienteNoBancoDeDados(7);
+    console.log(cliente);
+};
+
+processar();
+
