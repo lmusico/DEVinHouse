9bff0a0284667db9911401788a090483615c0e13
Lucas Músico <lucasmusico23@hotmail.com>
2023-02-18 22:37:10 -0300

    ex3

--- Módulo 1/Semana 07/EX1/exercicio/src/components/galeria/galeria.component.ts
+++ Módulo 1/Semana 07/EX1/exercicio/src/components/galeria/galeria.component.ts
@@ -7,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GaleriaComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+    
+   }
 
   ngOnInit(): void {
   }
