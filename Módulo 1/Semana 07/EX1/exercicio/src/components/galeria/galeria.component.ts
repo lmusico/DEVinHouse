694a31247a6f8df71d0f077d5e383d55a01db73d
Lucas Músico <lucasmusico23@hotmail.com>
2023-02-20 18:51:59 -0300

    botoes funcionando

--- Módulo 1/Semana 07/EX1/exercicio/src/components/galeria/galeria.component.ts
+++ Módulo 1/Semana 07/EX1/exercicio/src/components/galeria/galeria.component.ts
@@ -1,17 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-galeria',
   templateUrl: './galeria.component.html',
   styleUrls: ['./galeria.component.css']
 })
-export class GaleriaComponent implements OnInit {
+export class GaleriaComponent {
 
-  constructor() {
-    
-   }
+  public title = "Galeria de Imagens";
+  @Input() public fotos: string[] = [];
 
-  ngOnInit(): void {
+  public fotoAtual = 0;
+
+  public voltarParaPrimeira () {
+    this.fotoAtual = this.primeira()
+  }
+
+  public vaiParaUltima() {
+    this.fotoAtual = this.ultima();
+  } 
+
+  public primeira(){
+    return 0;
+  }
+
+  public ultima(){
+    return this.fotos.length - 1;
+  }
+  
+  public proximaFoto(){
+    this.fotoAtual ++;
+  }
+
+  public fotoAnterior(){
+    this.fotoAtual --;
   }
 
 }
