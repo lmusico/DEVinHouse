d429631609c33d3795c6c1c1d5073e10aaef1258
Lucas Músico <lucasmusico23@hotmail.com>
2023-04-11 19:25:25 -0300

    atu

---
+++ Módulo 2/Semana 2/Aula 1/meuPrimeirPrograma/Program.cs
@@ -0,0 +1,19 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Variáveis
+        String? nome; // tipo de dado texto
+
+        // Mensagens instrucionais ao usuário
+        Console.WriteLine("Olá mundo! Digite o seu nome: ");
+
+        // Entradas: inserção de dados
+        nome = Console.ReadLine(); // Aguardando o usuário digitar
+
+        // Saídas: mostrar resultados em tela
+        Console.WriteLine("Olá " + nome + "! Sejá bem bindo ao C#!");
+
+
+    }
+}
