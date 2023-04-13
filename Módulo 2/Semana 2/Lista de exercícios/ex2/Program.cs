152f72a628a42907a795c98d9751518ae3248687
Lucas Músico <lucasmusico23@hotmail.com>
2023-04-13 19:29:20 -0300

    ex2

---
+++ Módulo 2/Semana 2/Lista de exercícios/ex2/Program.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int num;
+        
+        Console.WriteLine("Entre com um número para descobrir se é par ou ímpar:");
+
+        num = int.Parse(Console.ReadLine());
+
+        if(num % 2 == 0){
+            Console.WriteLine("O número inserido é par");
+        }
+        else{
+            Console.WriteLine("O número inserido é ímpar");
+        }
+
+
+    }
+}
