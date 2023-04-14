dfe03a8d40dbeb1f2d354c90fc55debeb5231bc1
Lucas Músico <lucasmusico23@hotmail.com>
2023-04-13 21:25:18 -0300

    ex3

---
+++ Módulo 2/Semana 2/Lista de exercícios/ex3/Program.cs
@@ -0,0 +1,28 @@
+﻿internal partial class Program
+{
+    private static void Main(string[] args)
+    {
+        string nome;
+        int idade;
+        
+        Console.WriteLine("Digite seu nome:");
+        
+        nome = Console.ReadLine();
+
+        Console.WriteLine("Digite sua idade:");
+
+        idade = int.Parse(Console.ReadLine());
+
+        if(idade >= 18){
+            Console.WriteLine(nome+ ", você é maior de idade.");
+            if (idade >= 60){
+            Console.WriteLine("Você é considerado idoso.");
+        } 
+        }else {
+            Console.WriteLine(nome+ ", você é menor de idade.");
+        }
+
+
+
+    }
+}
