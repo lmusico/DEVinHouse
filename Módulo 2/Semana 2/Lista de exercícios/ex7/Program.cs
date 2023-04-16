59c266678c67b16dfd4bf5cdb07f4d7debc69121
Lucas Músico <lucasmusico23@hotmail.com>
2023-04-16 11:22:46 -0300

    ex7

---
+++ Módulo 2/Semana 2/Lista de exercícios/ex7/Program.cs
@@ -0,0 +1,19 @@
+﻿internal partial class Program
+{
+    private static void Main(string[] args)
+    {
+        int numero = 0;
+
+        do{
+        Console.WriteLine("Insira um número inteiro positivo:");
+        
+        numero = int.Parse(Console.ReadLine());
+        } while (numero<=0);
+
+        Console.WriteLine("Números pares de 0 a "+numero+":");
+
+        for(int i = 0; i<numero; i+=2){
+            Console.WriteLine(i);
+        }
+    }
+}
