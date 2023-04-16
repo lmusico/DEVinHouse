7e523dd11da46edbf7d26d1bd41bda2a0e195605
Lucas Músico <lucasmusico23@hotmail.com>
2023-04-16 09:50:15 -0300

    ex4

---
+++ Módulo 2/Semana 2/Lista de exercícios/ex4/Program.cs
@@ -0,0 +1,19 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string marca, modelo;
+        int km;
+
+        Console.WriteLine("Entre com a marca, modelo e quilometragem do carro:");
+        marca = Console.ReadLine();
+        modelo = Console.ReadLine();
+        km = int.Parse(Console.ReadLine());
+
+        Console.WriteLine(marca + ", " + modelo + ", " + km);
+        if (km>10000){
+            Console.WriteLine("É necessário realizar uma revisão");
+        }
+        
+    }
+}
