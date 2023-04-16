4c72bb163f9f9e9b8e4046c617be67a5a1bef318
Lucas Músico <lucasmusico23@hotmail.com>
2023-04-16 10:30:32 -0300

    ex5

---
+++ Módulo 2/Semana 2/Lista de exercícios/ex5/Program.cs
@@ -0,0 +1,22 @@
+﻿internal partial class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] numeros = new int[4];
+
+        for(int i = 0; i<4; i++){
+            Console.WriteLine("Entre com o número na posição " + i);
+            numeros[i] = int.Parse(Console.ReadLine());
+        }
+
+        Array.Sort(numeros);
+
+        Console.WriteLine("\nArray números em ordem crescente:");
+
+        foreach (var item in numeros)
+        {
+            Console.WriteLine(item);
+        }
+        
+    }
+}
