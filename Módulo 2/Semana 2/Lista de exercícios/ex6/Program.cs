f191a7c57eee3d5709c6878dfe89edaee20c072b
Lucas Músico <lucasmusico23@hotmail.com>
2023-04-16 11:04:15 -0300

    ex6

---
+++ Módulo 2/Semana 2/Lista de exercícios/ex6/Program.cs
@@ -0,0 +1,11 @@
+﻿internal partial class Program
+{
+    private static void Main(string[] args)
+    {
+        string frase = "A linguagem de programação C# é muito poderosa";
+
+        List<string>palavras = new List<string>(frase.Split(" "));
+
+        Console.WriteLine(palavras[^1]);
+    }
+}
