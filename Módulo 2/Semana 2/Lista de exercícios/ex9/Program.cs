db1785f2a45d1cae2d0e6d51ac16208ace3373b6
Lucas Músico <lucasmusico23@hotmail.com>
2023-04-16 15:02:30 -0300

    ex9

---
+++ Módulo 2/Semana 2/Lista de exercícios/ex9/Program.cs
@@ -0,0 +1,30 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int quantidade;
+        double soma = 0;
+        int[] numeros;
+        
+        Console.WriteLine("Quantos números deseja calcular a média aritimética?");
+        
+        quantidade = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Entre com os "+ quantidade + " números desejados:");
+
+        numeros = new int[quantidade];
+
+        for (int i = 0; i < quantidade; i++)
+        {
+            numeros[i] = int.Parse(Console.ReadLine());
+        }
+
+        foreach (var item in numeros)
+        {
+            soma += item;
+        }
+
+        Console.WriteLine("A média " + soma + "/" + quantidade + " é: " + (soma/quantidade));
+
+    }
+}
