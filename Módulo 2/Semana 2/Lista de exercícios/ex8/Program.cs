dd1a68a2266d1878da59112058560a31efcb9c9a
Lucas Músico <lucasmusico23@hotmail.com>
2023-04-16 14:42:25 -0300

    ex8

---
+++ Módulo 2/Semana 2/Lista de exercícios/ex8/Program.cs
@@ -0,0 +1,19 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int maior = 0;
+        int leitura = 0;
+        int repeticoes = 5;
+
+        Console.WriteLine("Digite cinco números e o maior deles será retornado");
+        
+        for(int i = 0; i<repeticoes; i++){
+            leitura = int.Parse(Console.ReadLine());
+            if (leitura > maior){
+                maior = leitura;
+            }
+        }
+        Console.WriteLine("O maior número digitado foi: "+maior);
+    }
+}
