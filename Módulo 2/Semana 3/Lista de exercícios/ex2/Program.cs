92f1cacb0523e6c62dd5717b12ea9b7972265281
Lucas Músico <lucasmusico23@hotmail.com>
2023-04-18 08:06:29 -0300

    ex2

---
+++ Módulo 2/Semana 3/Lista de exercícios/ex2/Program.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        Console.WriteLine(Soma(3, 4));
+
+        double resultado = Soma(3, 4, 5);
+
+        Console.WriteLine(resultado);
+    }
+    private static double Soma(double num1, double num2)
+    {
+        return num1 + num2;
+    }
+    private static double Soma(double num1, double num2, double num3)
+    {
+        return num1 + num2 + num3;
+    }
+}
