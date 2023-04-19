a6ff2a03e5d71aabae8fca55470ebfc071453b8e
Lucas Músico <lucasmusico23@hotmail.com>
2023-04-18 22:05:42 -0300

    ex 3 e ex 4

---
+++ Módulo 2/Semana 3/Lista de exercícios/ex3/Program.cs
@@ -0,0 +1,25 @@
+﻿using ProprietarioSpace;
+
+var proprietario1 = new Proprietario();
+var proprietario2 = new Proprietario();
+var proprietario3 = new Proprietario();
+
+proprietario1.nome = "Borion";
+
+proprietario2.nome = "Giborn";
+
+proprietario3.nome = "Dagusdir";
+
+proprietario1.cpf = "749.132.440-19";
+
+proprietario2.cpf = "858.316.640-40";
+
+proprietario3.cpf = "399.105.240-74";
+
+proprietario1.telefone = "123456";
+
+proprietario2.telefone = "321456";
+
+proprietario3.telefone = "123123";
+
+
---
+++ Módulo 2/Semana 3/Lista de exercícios/ex3/Proprietario.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProprietarioSpace
+{
+    public class Proprietario
+    {
+        public string nome;
+
+        public string cpf;
+
+        public string telefone;
+    }
+}
