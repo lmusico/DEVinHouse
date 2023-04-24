a95b96cb7824a3550c0abcd77834b3f65444f95a
Lucas Músico <lucasmusico23@hotmail.com>
2023-04-24 19:29:54 -0300

    ex6

---
+++ Módulo 2/Semana 3/Lista de exercícios/ex3/Carro.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarroSpace
+{
+    public class Carro
+    {
+        string marca;
+
+        string modelo;
+
+        string placa;
+
+        string cor;
+
+        string proprietario;
+    }
+}
