44f5c7e29a804bece38e5482dab7a8faafe78b81
Lucas Músico <lucasmusico23@hotmail.com>
2023-05-12 21:08:20 -0300

    ex6

---
+++ Módulo 2/Semana 6/Lista aula/ex6/Program.cs
@@ -0,0 +1,25 @@
+﻿
+
+Console.WriteLine("Entre com os números:");
+
+List<int> lista = new List<int>() {1, 6, 2, 4, 3, 8, -1, -5, 999};
+int menor;
+int maior;
+
+for (int i = 0; i < lista.Count; i++)
+{   
+    for (int j = i; j < lista.Count; j++){
+        while(lista[i] > lista[j]){
+            menor = lista[j];
+            maior = lista[i];
+            lista[i] = menor;
+            lista[j] = maior;
+        }
+  
+    }
+
+}
+foreach (var item in lista)
+{
+    Console.WriteLine(item);
+}
---
+++ Módulo 2/Semana 6/Lista de Exercícios/ex6/Program.cs
@@ -0,0 +1,53 @@
+﻿var lista = new List<int>();
+bool maisUmElemento = true;
+int maior, menor;
+
+Console.Clear();
+Console.WriteLine("Entre com um número para a lista ou dê enter para finalizar:");
+
+do
+{
+    try
+    {
+        int elemento = int.Parse(Console.ReadLine());
+        lista.Add(elemento);
+    }
+    catch (System.Exception)
+    {
+        Console.WriteLine("Finalizado a inserção de itens.");
+
+        foreach (var item in lista)
+        {
+            Console.WriteLine(item);
+        }
+
+        maisUmElemento = false;
+
+    }
+
+} while (maisUmElemento);
+
+for (int i = 0; i < lista.Count; i++)
+{
+    for (int j = i; j < lista.Count; j++)
+    {
+        // Console.WriteLine($"\nValores antes da comparação:\nValor de lista {i}: {lista[i]}\nValor de lista {j}: {lista[j]}");
+        while (lista[i] > lista[j])
+        {
+            menor = lista[j];
+            maior = lista[i];
+            lista[i] = menor;
+            lista[j] = maior;
+        }
+        // Console.WriteLine($"Valores depois da comparação:\nValor de lista {i}: {lista[i]}\nValor de lista {j}: {lista[j]}");
+    }
+
+    // Console.WriteLine("\n------------------------\n", lista[i]);
+}
+
+        Console.WriteLine("\n------------------------\nFinalizado a organização da lista:");
+
+        foreach (var item in lista)
+        {
+            Console.WriteLine(item);
+        }
